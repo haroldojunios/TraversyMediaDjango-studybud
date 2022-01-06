9f8f4017354178bf82f7d57582faf5498a40134f
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-06 18:50:31 -0300

    Create `update-room` view

--- base/views.py
+++ base/views.py
@@ -19,7 +19,20 @@ def createRoom(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            redirect('home')
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
