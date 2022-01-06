e8884093cc7f6e2a91e44e0b154cb5c9fcbc9cfb
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-06 12:14:11 -0300

    Create simple views for home and room

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+def home(req):
+    return HttpResponse('Home page')
+
+def room(req):
+    return HttpResponse('ROOM')
