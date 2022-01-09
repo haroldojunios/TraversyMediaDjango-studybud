a69b0db3895797f504cced19dad467aa2eba073d
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-09 18:38:33 -0300

    Create login view

--- base/views.py
+++ base/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.db.models import Q
 
 from .models import Room, Topic
 from .forms import RoomForm
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except BaseException:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+    context = {}
+    return render(request, 'base/login_reg.html', context)
 
 def home(request):
     query = request.GET.get('q', '')
