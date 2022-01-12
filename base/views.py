b22687f7cbb487ec4be63e10b9e3d71a02b1759e
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-11 21:35:18 -0300

    Implement user registration

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 
 from .models import Room, Topic
@@ -30,13 +31,30 @@ def login_page(request):
         else:
             messages.error(request, 'Username or password does not exist')
 
-    context = {}
+    context = {'page': 'login'}
     return render(request, 'base/login_reg.html', context)
 
 
 def logout_page(request):
     logout(request)
     return redirect('home')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request,'An error ocurred during registration')
+
+    form = UserCreationForm()
+    context = {'page': 'register', 'form': form}
+    return render(request, 'base/login_reg.html', context)
 
 
 def home(request):
