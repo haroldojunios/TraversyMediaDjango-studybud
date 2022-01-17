6395fa986d589dca01c950f72ed076f7e0017a66
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-16 21:30:53 -0300

    Create api view to get single room

--- base/api/views.py
+++ base/api/views.py
@@ -16,3 +16,10 @@ def get_rooms(request):
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_room(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
