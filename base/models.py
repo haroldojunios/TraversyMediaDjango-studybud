cc09b6ee48bad634c84558c25f22f766ab54f99e
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-17 15:24:19 -0300

    Add profile picture to user model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,6 +7,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default='avatar.svg')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
